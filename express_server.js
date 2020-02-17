c4700c246de95b3a04d794e110ddeed7b7aa4100
Steven Choi <stevencschoi87@gmail.com>
2020-02-17 22:33:27 +0000

    updated code to redirect to shorthand url

--- express_server.js
+++ express_server.js
@@ -29,6 +29,11 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = {
     shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]
