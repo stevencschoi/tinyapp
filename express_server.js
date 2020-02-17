b484b279cbbcfc5e9b84ba1a709d5277893ac43d
Steven Choi <stevencschoi87@gmail.com>
2020-02-17 18:44:20 +0000

    added ejs tutorial templates

--- express_server.js
+++ express_server.js
@@ -2,21 +2,44 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+// app.get("/", (req, res) => {
+//   res.send("Hello!");
+// });
+
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
+
+// index page 
+app.get('/', function(req, res) {
+  const drinks = [
+    { name: 'Sake Bomb', drunkness: 3 },
+    { name: 'Absinthe', drunkness: 5 },
+    { name: 'Bacardi 151', drunkness: 10 }
+  ];
+  const tagline = "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
+
+  res.render('pages/index', {
+    drinks,
+    tagline
+  });
+});
+
+// about page 
+app.get('/about', function(req, res) {
+  res.render('pages/about');
 });
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
