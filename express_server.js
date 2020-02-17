b49ec5c0f3a1fe01ec3385034b25464a60f228c4
Steven Choi <stevencschoi87@gmail.com>
2020-02-17 19:02:25 +0000

    removed ejs tutorial files into separate directory

--- express_server.js
+++ express_server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-// set the view engine to ejs
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
