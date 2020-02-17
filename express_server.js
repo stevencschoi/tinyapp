0faa10181adab3dae6418c0abba3fd2c61fc1b42
Steven Choi <stevencschoi87@gmail.com>
2020-02-17 18:59:21 +0000

    removed ejs tutorial code into separate exercise folder

--- express_server.js
+++ express_server.js
@@ -10,36 +10,16 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-// app.get("/", (req, res) => {
-//   res.send("Hello!");
-// });
-
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body>Hello <b>World</b></body></html>\n");
-// });
-
-// index page 
-app.get('/', function(req, res) {
-  const drinks = [
-    { name: 'Sake Bomb', drunkness: 3 },
-    { name: 'Absinthe', drunkness: 5 },
-    { name: 'Bacardi 151', drunkness: 10 }
-  ];
-  const tagline = "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
-
-  res.render('pages/index', {
-    drinks,
-    tagline
-  });
-});
-
-// about page 
-app.get('/about', function(req, res) {
-  res.render('pages/about');
+app.get("/", (req, res) => {
+  res.send("Hello!");
 });
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
