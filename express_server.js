cbbafb598ae2bace329be69e6c77549e1b0544b4
Steven Choi <stevencschoi87@gmail.com>
2020-02-18 21:31:42 +0000

    changed styling and modified conditional

--- express_server.js
+++ express_server.js
@@ -85,6 +85,11 @@ app.post('/login', (req, res) => {
   res.redirect('/urls');
 });
 
+app.post('/logout', (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/urls');
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
