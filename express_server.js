01e489e92d7dab6c6a9caee1f22ae866d358025a
Steven Choi <stevencschoi87@gmail.com>
2020-02-18 17:41:46 +0000

    added delete functionality for tinyurl

--- express_server.js
+++ express_server.js
@@ -1,49 +1,55 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const PORT = 8080;
 
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  'b2xVn2': 'http://www.lighthouselabs.ca',
+  '9sm5xK': 'http://www.google.com'
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+app.get('/', (req, res) => {
+  res.send('Hello!');
 });
 
-app.get("/urls.json", (req, res) => {
+app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/urls", (req, res) => {
+app.get('/urls', (req, res) => {
   let templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
+  res.render('urls_index', templateVars);
 });
 
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
 });
 
-app.get("/u/:shortURL", (req, res) => {
+app.get('/u/:shortURL', (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
-app.get("/urls/:shortURL", (req, res) => {
+app.get('/urls/:shortURL', (req, res) => {
   let templateVars = {
     shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]
   };
-  res.render("urls_show", templateVars);
+  res.render('urls_show', templateVars);
 });
 
-app.post("/urls", (req, res) => {
+app.post('/urls', (req, res) => {
   urlDatabase[generateRandomString()] = req.body.longURL;
   console.log(req.body);  // Log the POST request body to the console
+  res.redirect('/urls');
+});
+
+app.post('/urls/:shortURL/delete', (req, res) => {
+  // console.log(req.params.shortURL);
+  delete urlDatabase[req.params.shortURL];
   res.redirect('/urls');
 });
 
@@ -56,7 +62,7 @@ const generateRandomString = () => {
   return result;
 };
 
-// app.get("/hello", (req, res) => {
+// app.get('/hello', (req, res) => {
 //   res.send("<html><body>Hello <b>World</b></body></html>\n");
 // });
 
